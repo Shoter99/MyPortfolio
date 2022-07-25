3121f04484c1ee209b6244db4248153e297218b8
Shoter99 <shoter998@gmail.com>
2022-07-25 11:44:13 +0200

    added responsive layout and scroll snap

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import TitlePageBtn from './components/TitlePageBtn';
 
 function App() {
   return (
-    <div>
-      <div className='flex direction-row justify-evenly items-center h-screen'>
-        <div>
-          <span className='text-7xl text-neutral-700'>Dawid</span>
+    <div className="snap-y snap-mandatory h-screen overflow-scroll overflow-x">
+      <section id="home" className='snap-start flex md:flex-row flex-col justify-center md:justify-evenly items-center h-screen'>
+        <div className='text-center md:text-left'>
+          <span className='md:text-7xl text-4xl text-neutral-700'>Dawid</span>
           <br />
-          <span className='text-orange-500 text-8xl'>Roszman</span>
+          <span className='text-orange-500 text-5xl md:text-8xl'>Roszman</span>
         </div>
-        <div>
+        <div className="flex md:block p-3 md:p-0">
           <TitlePageBtn text="Projects" />
           <div className='p-2'></div>
           <TitlePageBtn text="Resume" />
         </div>
-      </div>
+      </section>
+      <section id="aboutme" className='snap-start h-screen bg-neutral-700'>
+
+      </section>
+      <section id="projects" className='snap-start h-screen bg-neutral-200'>
+
+      </section>
+      
     </div>
   );
 }
--- src/components/TitlePageBtn.tsx
+++ src/components/TitlePageBtn.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TitlePageBtn = ({ text }: { text: string }) => {
     return (
         <button className='border rounded border-neutral-700 p-3 border-2 text-neutral-700 hover:text-neutral-200 hover:bg-neutral-700 transition duration-500'>
-            <span className='font-bold text-3xl'>{text}</span>
+            <span className='font-bold md:text-3xl'>{text}</span>
         </button>
     )
 }
