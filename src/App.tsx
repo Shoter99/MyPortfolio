608357dd2fecc37f4e8bae68c86700f183187a9e
Shoter99 <shoter998@gmail.com>
2022-07-25 18:47:24 +0200

    added navigation between sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './App.css';
+import ArrowUp from './utils/uparrow.svg';
 import TitlePageBtn from './components/TitlePageBtn';
 
 function App() {
+  
+  const goToTop = () => {
+    const element = document.querySelector(`#home`)
+    element?.scrollIntoView({behavior: 'smooth', block: 'center'})
+
+  }
+  
+  
+
+
   return (
     <div className="snap-y snap-mandatory h-screen overflow-scroll overflow-x">
       <section id="home" className='snap-start flex md:flex-row flex-col justify-center md:justify-evenly items-center h-screen'>
@@ -12,9 +23,9 @@ function App() {
           <span className='text-orange-500 text-5xl md:text-8xl'>Roszman</span>
         </div>
         <div className="flex md:block p-3 md:p-0">
-          <TitlePageBtn text="Projects" />
+          <TitlePageBtn text="Resume" link='aboutme' />
           <div className='p-2'></div>
-          <TitlePageBtn text="Resume" />
+          <TitlePageBtn link="projects" text="Projects" />
         </div>
       </section>
       <section id="aboutme" className='snap-start h-screen bg-neutral-700'>
@@ -22,8 +33,10 @@ function App() {
       </section>
       <section id="projects" className='snap-start h-screen bg-neutral-200'>
 
-      </section>
-      
+      </section> 
+          <button onClick={goToTop} className='transition duration-1000 absolute right-0 bottom-0 w-8 h-8 m-4 md:m-10 md:w-12 md:h-12 p-1 flex rounded-full justify-center items-center md:hover:w-14 md:hover:h-14'>
+          <img src={ArrowUp} alt="Arrow Up"/>
+          </button>
     </div>
   );
 }
