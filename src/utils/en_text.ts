9aafa041a69e9f32775d51a7b7549fcf34c65d17
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2023-02-05 13:08:22 +0100

    query data from db

--- src/utils/en_text.ts
+++ src/utils/en_text.ts
@@ -1,106 +1,24 @@
 export const main_text_en = {
-    my_resume: "My Resume",
-    my_projects: "My Projects",
-    about_me: "About me",
+  my_resume: "",
+  my_projects: "",
+  about_me: "",
+};
+export const about_me_text_en = {
+  text: "",
+};
+
+interface project {
+  id: number;
+  category: string;
+  description: string;
+  name: string;
+  links: link[];
 }
-export const projects = [
-    {
-        id: 5,
-        category: "Web Development",
-        name: "Pentago",
-        description: "Web application created using ReactJs, copy of board game Pentago with a twist.",
-        links: [
-            {
-                name: "Check on Github",
-                url: "https://github.com/Shoter99/Pentago",
-            },
-            {
-                name: "Live version",
-                url: "https://shoter99.github.io/Pentago"
-            }
-        ]   
-    },
-    {
-        id: 0,
-        category: "Game Development",
-        name: "Minute to Evolve",
-        description: "Game created in Godot. 2D Platformer where you need to collect points to evolve and get new powers. Made for Go Godot 3 in a week.",
-        links: [
-            {
-                name: "Check on Github",
-                url: "https://github.com/Shoter99/GameJamFirst",
-            },
-            {
-                name: "Live version",
-                url: "https://shoter99.itch.io/minute-to-evolve",
-            }
-    ]
-    },
-    {
-        id: 1,
-        category: "Game Development",
-        name: "Go Deeper",
-        description: "My first game created using Unity for LudumDare48. Simple 2D platformer but instead of jumping up you are falling deeper and deeper.",
-        links: [
-            {
-                name: "Check on Github",
-                url: "https://github.com/Shoter99/LudumDare48",
-            },
-            {
-                name: "Download for mobile",
-                url: "https://drive.google.com/file/d/1CoKcpfbykTqbgO17XORe-r-Q3tE6ckLc/view?usp=sharing"
-            }
-        ]   
-    },
-    {
-        id: 2,
-        category: "Web Development",
-        name: "Wordle Clone",
-        description: "As name suggested it is Wordle Clone created with React and it is fully responsive.",
-        links: [
-            {
-                name: "Check on Github",
-                url: "https://github.com/Shoter99/WordleClone_React",
-            },
-            {
-                name: "Live version",
-                url: "https://shoter99.github.io/WordleClone_React",
-            }
-        ]   
-    },
-    {
-        id: 3,
-        category: "Web Development",
-        name: "Guess the flag",
-        description: "Fully responsive web app created with React. It's using external api in order to extract data for the quiz.",
-        links: [
-            {
-                name: "Check on Github",
-                url: "https://github.com/Shoter99/CountryFlag_Quiz_ReactApp",
-            },
-            {
-                name: "Live version",
-                url: "https://shoter99.github.io/CountryFlag_Quiz_ReactApp",
-            }
-        ]   
-    },
-    {
-        id: 4,
-        category: "Mobile Application",
-        name: "Sudoku Solver",
-        description: "Mobile App created with React Native where you can take a photo of a sudoku board and then the photo is send to server where sudoku is solved and results are send back to app. Server is created using Python and FastAPI. Server is using Tesorflow to read numbers from photo.",
-        links: [
-            {
-                name: "Check on Github",
-                url: "https://github.com/Shoter99/SudokuSolverWithOpenCV",
-            },
-        ]   
-    },   
-    
-
-]
-
-
+interface link {
+  name: string;
+  url: string;
+}
+export const projects: project[] = [];
 
 // {
 //     id: 0,
