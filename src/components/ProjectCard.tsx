e2354ef4824e05c51a9acac65f60f5813a399613
Dawid Roszman <dawidroszman@gmail.com>
2023-05-27 10:37:17 +0200

    changed bg on projects section

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,38 +17,43 @@ interface Props {
 
 const ProjectCard = (props: Props) => {
   return (
-    <div data-aos={props.index % 2 === 0 ? "fade-right" : "fade-left"} className="border border-neutral-700 rounded shadow-2xl flex flex-col md:flex-row justify-between md:items-center  text-neutral-200 bg-neutral-700">
-      <div className="md:w-[600px] p-10">
-        <div className="md:flex justify-between">
-          <h2 className="font-bold text-xl notranslate">{props.values.name}</h2>
-          <br />
-          <h3 className="font-bold text-xl notranslate">
-            {props.values.category}
-          </h3>
-        </div>
-        <br />
-        <h3 className="text-justify">{props.values.description}</h3>
-      </div>
-      <div className="mx-4 h-[80%] rounded border-x-2 hidden md:block border border-slate-200 w-[0px]"></div>
-      <div className="py-5 md:mt-0 px-10 w-96">
-        {props.values.links.map((val, id) => (
-          <div key={id}>
-            <h2>
-              {val.name}
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={val.url}
-                className="text-orange-400 italic pl-2 hover:underline"
-              >
-                {props.here}
-              </a>
+    <div
+      data-aos={props.index % 2 === 0 ? "fade-right" : "fade-left"}
+      className="project-bg border border-neutral-700 rounded shadow-2xl flex flex-col md:flex-row justify-between md:items-center  text-neutral-200 bg-neutral-700"
+    >
+        <div className="md:w-[600px] p-10">
+          <div className="md:flex justify-between">
+            <h2 className="font-bold text-xl notranslate">
+              {props.values.name}
             </h2>
             <br />
+            <h3 className="font-bold text-xl notranslate">
+              {props.values.category}
+            </h3>
           </div>
-        ))}
+          <br />
+          <h3 className="text-justify">{props.values.description}</h3>
+        </div>
+        <div className="mx-4 h-[80%] rounded border-x-2 hidden md:block border border-slate-200 w-[0px]"></div>
+        <div className="py-5 md:mt-0 px-10 w-96">
+          {props.values.links.map((val, id) => (
+            <div key={id}>
+              <h2>
+                {val.name}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={val.url}
+                  className="text-orange-400 italic pl-2 hover:underline"
+                >
+                  {props.here}
+                </a>
+              </h2>
+              <br />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
   );
 };
 
--- src/components/TitlePageBtn.tsx
+++ src/components/TitlePageBtn.tsx
@@ -7,9 +7,10 @@ const TitlePageBtn = ({ text, link, animation }: { text: string; link: string, a
 
   return (
     <button
-      data-aos={animation}
+      // data-aos={animation}
       onClick={scrollToId}
-      className="border rounded bg-neutral-200 border-neutral-700 p-3 border-2 text-neutral-700 hover:text-neutral-200 hover:bg-neutral-700 transition duration-500 shadow-md"
+      className="title-transition rounded bg-neutral-200 border-neutral-700
+      p-3 border-2 text-neutral-700 hover:text-neutral-200 hover:bg-neutral-700 shadow-md"
     >
       <span className="font-bold md:text-3xl">{text}</span>
     </button>
